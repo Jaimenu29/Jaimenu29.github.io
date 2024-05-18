e0b42314afd3660efc6e6563ccaa89c8d9944352
Jaimenu29 <147121489+Jaimenu29@users.noreply.github.com>
2024-05-18 16:51:43 -0400

    resultado final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //total preguntas del juego
 const TOTAL_PREGUNTAS = 10;
 //tiempo del juego
-const TIEMPO_DEL_JUEGO = 60;
+const TIEMPO_DEL_JUEGO = 100;
 //estructura para almacenar las preguntas
 const bd_juego = [
   {
